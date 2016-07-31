0ee1a83bc1e3ab7fb9b092df67ba24e762539d90
Daniel Leining <daniel@leining.org>
2016-07-31 05:14:41 -0400

    only include bluetooth.c if bluetooth is enable

--- tmk_core/protocol/chibios.mk
+++ tmk_core/protocol/chibios.mk
@@ -4,7 +4,10 @@ CHIBIOS_DIR = $(PROTOCOL_DIR)/chibios
 
 SRC += $(CHIBIOS_DIR)/usb_main.c
 SRC += $(CHIBIOS_DIR)/main.c
-SRC += $(CHIBIOS_DIR)/bluetooth.c
+
+ifdef BLUETOOTH_ENABLE
+	SRC += $(CHIBIOS_DIR)/bluetooth.c
+endif
 
 VPATH += $(TMK_PATH)/$(PROTOCOL_DIR)
 VPATH += $(TMK_PATH)/$(CHIBIOS_DIR)
